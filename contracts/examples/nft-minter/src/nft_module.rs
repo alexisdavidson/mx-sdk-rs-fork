51de01d78e84592f080f69c341a740bd04bd4944
Alexis Davidson <alexisdavidson00@gmail.com>
2023-02-12 19:55:51 +0700

    ts

--- contracts/examples/nft-minter/src/nft_module.rs
+++ contracts/examples/nft-minter/src/nft_module.rs
@@ -1,6 +1,9 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
+extern crate alloc;
+use alloc::string::ToString;
+
 const NFT_AMOUNT: u32 = 1;
 const ROYALTIES_MAX: u32 = 10_000;
 
@@ -202,9 +205,9 @@ pub trait NftModule {
         let name = self.nft_name_prefix().get(); // todo: append current_nft_id
         let royalties = self.royalties().get();
 
-        // let s = &current_nft_id.to_string();
+        let s = current_nft_id.to_string();
 
-        let message = sc_format!("Hello {} world", current_nft_id);
+        // let message = sc_format!("Hello {} world", current_nft_id);
 
         // let current_nft_id_bytes = current_nft_id.to_be_bytes();
         // name.append_bytes(&current_nft_id_bytes);
